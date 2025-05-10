9fcc0e5a1dcca8a11d276e5c3ac3ffed6bcaadbc
DAVRONBEK ABDURAZZOQOV <94719744+21040001@users.noreply.github.com>
2025-05-10 14:06:36 +0300

    Update diabet.py

--- diabet.py
+++ diabet.py
@@ -1,30 +1,23 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Yangi import
 import pickle
 import numpy as np
 
 # Flask ilovasini yaratish
 app = Flask(__name__)
+CORS(app)  # Barcha domenlardan so‘rovlarni qabul qilishga ruxsat beradi
 
 # Modelni yuklash
 with open('diabetes_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
-
 # API endpoint: Bashorat qilish
 @app.route('/predict', methods=['POST'])
 def predict():
-    # So'rovda yuborilgan ma'lumotlarni olish
     data = request.get_json(force=True)
-
-    # Ma'lumotlar formati (masalan, yangi ma'lumotlar uchun 8 ta xususiyat)
-    features = np.array([data['features']])  # request body'dan ma'lumot olish
-
-    # Modelni ishlatib bashorat qilish
+    features = np.array([data['features']])
     prediction = model.predict(features)
-
-    # Natijani JSON formatda qaytarish
     return jsonify({'prediction': prediction.tolist()})
-
 
 # Backendni ishga tushirish
 if __name__ == '__main__':
